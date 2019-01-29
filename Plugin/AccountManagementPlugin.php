b0659206acc23fadf44d5fb12da9f6dc5b8d6393
Siniša Čolić <sinisa.colic@gmail.com>
2019-01-29 20:52:38 +0100

    Add support for changing and resetting customer password

--- Plugin/AccountManagementPlugin.php
+++ Plugin/AccountManagementPlugin.php
@@ -24,6 +24,66 @@ class AccountManagementPlugin
     }
 
     /**
+     * @param string|null $password
+     */
+    private function dispatchPasswordCheckEvent($password)
+    {
+        if (!is_null($password)) {
+            $this->eventManager->dispatch(
+                'timpack_pwnedvalidator_check_password_strength',
+                [
+                    'password' => $password,
+                ]
+            );
+        }
+    }
+
+    /**
+     * @param AccountManagementInterface $subject
+     * @param string $email
+     * @param string $resetToken
+     * @param string $newPassword
+     */
+    public function beforeResetPassword(
+        AccountManagementInterface $subject,
+        $email,
+        $resetToken,
+        $newPassword
+    ) {
+        $this->dispatchPasswordCheckEvent($newPassword);
+    }
+
+    /**
+     * @param AccountManagementInterface $subject
+     * @param string $email
+     * @param string $currentPassword
+     * @param string $newPassword
+     */
+    public function beforeChangePassword(
+        AccountManagementInterface $subject,
+        $email,
+        $currentPassword,
+        $newPassword
+    ) {
+        $this->dispatchPasswordCheckEvent($newPassword);
+    }
+
+    /**
+     * @param AccountManagementInterface $subject
+     * @param string $customerId
+     * @param string $currentPassword
+     * @param string $newPassword
+     */
+    public function beforeChangePasswordById(
+        AccountManagementInterface $subject,
+        $customerId,
+        $currentPassword,
+        $newPassword
+    ) {
+        $this->dispatchPasswordCheckEvent($newPassword);
+    }
+
+    /**
      * @param AccountManagementInterface $subject
      * @param CustomerInterface $customer
      * @param null $password
@@ -35,13 +95,6 @@ class AccountManagementPlugin
         $password = null,
         $redirectUrl = ''
     ) {
-        if (!is_null($password)) {
-            $this->eventManager->dispatch(
-                'timpack_pwnedvalidator_check_password_strength',
-                [
-                    'password' => $password,
-                ]
-            );
-        }
+        $this->dispatchPasswordCheckEvent($password);
     }
 }
